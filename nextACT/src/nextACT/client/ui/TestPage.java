54fea1a1aec7e78d650a05892205f547f990b69b
nactified <nactified@maltebehrendt.de>
2011-03-24 20:40:14 +0000

    Ups, forgot to upload it before the vacations... Added an interface for testing the RequestFactory elements (which are based on a graph db on server side). It's not wired yet though..

--- nextACT/src/nextACT/client/ui/TestPage.java
+++ nextACT/src/nextACT/client/ui/TestPage.java
@@ -10,11 +10,33 @@ import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HasText;
 import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.LongBox;
+import com.google.gwt.user.client.ui.SimpleCheckBox;
+import com.google.gwt.user.datepicker.client.DateBox;
+import com.google.gwt.user.client.ui.DoubleBox;
+import com.google.gwt.user.client.ui.IntegerBox;
 
-public class TestPage extends Composite implements HasText {
+public class TestPage extends Composite {
 
 	private static TestPageUiBinder uiBinder = GWT
 			.create(TestPageUiBinder.class);
+	@UiField Button createButton;
+	@UiField Button saveButton;
+	@UiField TextBox loadID;
+	@UiField LongBox id;
+	@UiField LongBox facebookID;
+	@UiField LongBox version;
+	@UiField SimpleCheckBox isNative;
+	@UiField TextBox name;
+	@UiField TextBox description;
+	@UiField TextBox locationName;
+	@UiField TextBox locationCity;
+	@UiField DateBox startDate;
+	@UiField DateBox endDate;
+	@UiField DoubleBox longitude;
+	@UiField DoubleBox latitude;
+	@UiField IntegerBox participantLimit;
 
 	interface TestPageUiBinder extends UiBinder<Widget, TestPage> {
 	}
@@ -23,25 +45,11 @@ public class TestPage extends Composite implements HasText {
 		initWidget(uiBinder.createAndBindUi(this));
 	}
 
-	@UiField
-	Button button;
-
 	public TestPage(String firstName) {
 		initWidget(uiBinder.createAndBindUi(this));
-		button.setText(firstName);
 	}
 
-	@UiHandler("button")
-	void onClick(ClickEvent e) {
-		Window.alert("Hello!");
+	@UiHandler("createButton")
+	void onCreateButtonClick(ClickEvent event) {
 	}
-
-	public void setText(String text) {
-		button.setText(text);
-	}
-
-	public String getText() {
-		return button.getText();
-	}
-
 }
